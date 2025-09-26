cc29507ef4c02d46c2017122f22ca2e8e106aa2f
Aditya-gautam21 <153100646+Aditya-gautam21@users.noreply.github.com>
2025-09-26 20:10:23 +0530

    update

---
+++ backend/agents.py
@@ -0,0 +1,11 @@
+import os
+import datetime
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+def get_calender_service():
+    
